238d35fb849d75e2d163bb4e6427e93b5e07d358
conghuy20hn <conghuy20hn@gmail.com>
2019-10-09 22:44:13 +0700

    trang tin tuc

---
+++ apps/frontend/modules/pageNews/templates/detailSuccess.php
@@ -0,0 +1,81 @@
+﻿<?php
+?>
+
+<!--breadcrumbs area start-->
+<div class="breadcrumbs_area">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="breadcrumb_content">
+                    <ul>
+                        <li><a href="<?php echo url_for1('homepage') ?>"><?php echo __('home') ?></a></li>
+                        <li><a href="<?php echo url_for1('hq_news') ?>"><?php echo __('News') ?></a></li>
+                        <li><?php echo htmlentities($new->title) ?></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--breadcrumbs area end-->
+
+<!--blog body area start-->
+<div class="blog_details blog_padding mt-23">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-9 col-md-12">
+                <!--blog grid area start-->
+                <div class="blog_details_wrapper">
+                    <!--                    <div class="blog_thumb">-->
+                    <!--                        <a href="#"><img src="assets/img/blog/blog-big1.jpg" alt=""></a>-->
+                    <!--                    </div>-->
+                    <div class="blog_content">
+                        <h3 class="post_title"><?php echo htmlentities($new->title) ?></h3>
+
+                        <div class="post_meta">
+                            <!--                            <span><i class="ion-person"></i> Posted by </span>-->
+                            <!--                            <span><a href="#">admin</a></span>-->
+                            <!--                            <span>|</span>-->
+                            <span><i class="fa fa-calendar"
+                                     aria-hidden="true"></i>  Posted on <?php echo date('Y-m-d', strtotime($new->published_time)) ?>	</span>
+
+                        </div>
+                        <div class="post_content">
+
+                            <?php echo $new->body ?>
+                        </div>
+
+                    </div>
+
+                </div>
+                <!--blog grid area start-->
+            </div>
+            <div class="col-lg-3 col-md-12">
+                <div class="blog_sidebar_widget">
+                    <div class="widget_list widget_search">
+                        <h3><?php echo __('Search') ?></h3>
+
+                        <form action="<?php echo url_for1('@hq_news_search') ?>">
+                            <input placeholder="<?php echo __('Search') ?>..." type="text" name="q">
+                            <button type="submit"><?php echo __('Search') ?></button>
+                        </form>
+                    </div>
+                    <?php include_component('pageNews', 'hotNew') ?>
+
+                    <!--                    <div class="widget_list widget_categories">-->
+                    <!--                        <h3>Categories</h3>-->
+                    <!--                        <ul>-->
+                    <!--                            <li><a href="#">Fashion</a></li>-->
+                    <!--                            <li><a href="#">Travel</a></li>-->
+                    <!--                            <li><a href="#">Videos</a></li>-->
+                    <!--                            <li><a href="#">WordPress</a></li>-->
+                    <!--                        </ul>-->
+                    <!--                    </div>-->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--blog section area end-->
+
---
+++ apps/frontend/modules/pageNews/templates/searchNewSuccess.php
@@ -0,0 +1,113 @@
+﻿<?php
+$news = $pager->getResults();
+?>
+
+<!--breadcrumbs area start-->
+<div class="breadcrumbs_area">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="breadcrumb_content">
+                    <ul>
+                        <li><a href="<?php echo url_for1('homepage') ?>"><?php echo __('home') ?></a></li>
+                        <li><?php echo __('Search') ?></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--breadcrumbs area end-->
+
+<!--blog area start-->
+<div class="blog_page_section mt-23">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="blog_wrapper">
+                    <?php
+                    if ($pager->count() > 0) {
+                        foreach ($news as $new) {
+                            $pathImg = '/uploads/' . sfConfig::get('app_article_images') . $new->image_path;
+                            ?>
+                            <div class="single_blog">
+                                <div class="blog_thumb">
+                                    <a href="<?php echo url_for1('@hq_new_detail?slug=' . $new->slug) ?>">
+                                        <img src="<?php echo VtHelper::getThumbUrl($pathImg, 356, 245) ?>" alt=""></a>
+                                </div>
+                                <div class="blog_content">
+                                    <h3><a href="<?php echo url_for1('@hq_new_detail?slug=' . $new->slug) ?>">
+                                            <?php echo $new->title ?>
+                                        </a></h3>
+
+                                    <div class="blog_meta">
+                                        <span class="post_date"><i class="fa-calendar fa"></i>
+                                            <?php echo date('Y-m-d', strtotime($new->published_time)) ?>
+                                        </span>
+                                        <!--                                        <span class="author"><i class="fa fa-user-circle"></i> Posts by : admin</span>-->
+                                        <!--                                    <span class="category">-->
+                                        <!--                                        <i class="fa fa-folder-open"></i>-->
+                                        <!--                                        <a href="#">Fashion</a>-->
+                                        <!--                                    </span>-->
+                                    </div>
+                                    <div class="blog_desc">
+                                        <p>
+                                            <?php echo $new->header ?>
+                                        </p>
+                                    </div>
+                                    <div class="readmore_button">
+                                        <a href="<?php echo url_for1('@hq_new_detail?slug=' . $new->slug) ?>">
+                                            <?php echo __('read more') ?>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <?php
+                        }
+                    } else {
+                        echo "<h3>" . __('Data not found') . "</h3>";
+                    }
+                    ?>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--blog area end-->
+
+<!--blog pagination area start-->
+<?php if ($pager && $pager->haveToPaginate()) { ?>
+    <div class="blog_pagination">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="pagination">
+                        <ul>
+                            <?php if ($pager->getPage() >= 2): ?>
+                                <li><a href="<?php echo url_for1('@hq_news_search?q=' . htmlentities($queryName)) ?>">
+                                        <i class="fa fa-angle-left"></i></a></li>
+                            <?php endif; ?>
+                            <?php foreach ($pager->getLinks() as $page): ?>
+                                <li class="<?php echo $page == $pager->getPage() ? 'current' : '' ?>">
+                                    <a href="<?php echo url_for1('@hq_news_search?page=' . $page . '&q=' . htmlentities($queryName)) ?>">
+                                        <?php echo $page ?></a></li>
+                            <?php endforeach; ?>
+                            <?php if ($pager->getPage() < $pager->getLastPage()): ?>
+                                <li>
+                                    <a href="<?php echo url_for1('@hq_news_search?page=' . $pager->getLastPage() . '&q=' . htmlentities($queryName)) ?>"><i
+                                            class="fa fa-angle-right"></i></a></li>
+                            <?php endif; ?>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php } ?>
+<!--blog pagination area end-->
+
+<!--slider area end-->
+
+<!-- modal area end-->
